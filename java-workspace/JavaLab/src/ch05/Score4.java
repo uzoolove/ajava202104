493cf4a9dbee82ee7f577b9e0f91fcc89cd1a42c
KilyongJeong <aceppin@motizensoft.com>
2021-04-27 10:37:45 +0900

    .

--- java-workspace/JavaLab/src/ch05/Score4.java
+++ java-workspace/JavaLab/src/ch05/Score4.java
@@ -5,6 +5,32 @@ public class Score4 {
 	private int eng;
 	private int math;
 	
+	
+	
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		if(eng > 100) {
+			eng = 100;
+		}
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	// setter
+	public void setKor(int kor) {		
+		this.kor = kor;
+	}
+	// getter
+	public int getKor() {
+		return this.kor;
+	}
+	
 	public int sum() {
 		int total = kor + eng + math;
 		return total;
