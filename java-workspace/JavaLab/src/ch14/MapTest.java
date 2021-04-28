5987a665001aced6adf738df82f1acde87adc318
KilyongJeong <aceppin@motizensoft.com>
2021-04-28 15:18:09 +0900

    .

--- java-workspace/JavaLab/src/ch14/MapTest.java
+++ java-workspace/JavaLab/src/ch14/MapTest.java
@@ -1,11 +1,14 @@
 package ch14;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class MapTest {
 	public static void main(String[] args) {
-		Map<String, String> fruits = new HashMap<>();
+//		Map<String, String> fruits = new HashMap<>();
+//		Map<String, String> fruits = new LinkedHashMap<>();
+		Map<String, String> fruits = new TreeMap<>();
 		
 		fruits.put("월", "바나나");
 		fruits.put("화", "딸기");
@@ -14,5 +17,29 @@ public class MapTest {
 		fruits.put("화", "파인애플");
 		fruits.put("금", "사과");
 		fruits.put("토", "자몽");
+		
+		System.out.println("목: " + fruits.get("목"));
+		
+		printFruits(fruits);
+	}
+	
+	static void printFruits(Map<String, String> fruits) {
+		Set<String> keySet = fruits.keySet();
+		for(String key : keySet) {
+			System.out.println(key + ": " + fruits.get(key));
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
