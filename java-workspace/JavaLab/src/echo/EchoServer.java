58c0960c507ce4aa7ad5bd7d0106ae10a649a9de
KilyongJeong <aceppin@motizensoft.com>
2021-04-29 13:02:42 +0900

    .

--- java-workspace/JavaLab/src/echo/EchoServer.java
+++ java-workspace/JavaLab/src/echo/EchoServer.java
@@ -2,6 +2,9 @@ package echo;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -13,12 +16,22 @@ public class EchoServer {
 	private void startServer(){
 		try{
 			// 서버소켓 생성
+			ss = new ServerSocket(1234);
+			System.out.println("서버 구동 완료. 1234");
 			
 			// 클라이언트 접속 대기
+			s = ss.accept();
+			System.out.println("클라이언트 접속. " + s.getInetAddress().getHostAddress());
 			
 			// 소켓에서 입출력 스트림 생성
+			in = s.getInputStream();
+			out = s.getOutputStream();			
 			
 			// 입력 스트림에서 읽고 출력 스트림으로 전송한다.
+			int readData = 0;
+			while((readData = in.read()) != -1) {
+				out.write(readData);
+			}
 			
 		}catch(Exception e){
 			e.printStackTrace();
