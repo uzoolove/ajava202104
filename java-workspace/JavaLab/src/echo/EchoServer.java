8c75559472db385c868a39fca40075b2732f90a6
KilyongJeong <aceppin@motizensoft.com>
2021-04-29 13:31:57 +0900

    .

--- java-workspace/JavaLab/src/echo/EchoServer.java
+++ java-workspace/JavaLab/src/echo/EchoServer.java
@@ -9,28 +9,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class EchoServer {
-  private ServerSocket ss;
-  private Socket s;
-  private InputStream in;
-  private OutputStream out;
+	private ServerSocket ss;
 	private void startServer(){
 		try{
 			// 서버소켓 생성
 			ss = new ServerSocket(1234);
 			System.out.println("서버 구동 완료. 1234");
 			
-			// 클라이언트 접속 대기
-			s = ss.accept();
-			System.out.println("클라이언트 접속. " + s.getInetAddress().getHostAddress());
-			
-			// 소켓에서 입출력 스트림 생성
-			in = s.getInputStream();
-			out = s.getOutputStream();			
-			
-			// 입력 스트림에서 읽고 출력 스트림으로 전송한다.
-			int readData = 0;
-			while((readData = in.read()) != -1) {
-				out.write(readData);
+			while(true) {
+				// 클라이언트 접속 대기
+				Socket s = ss.accept();
+				System.out.println("클라이언트 접속. " + s.getInetAddress().getHostAddress());
+				EchoServerThread st = new EchoServerThread(s);
+				st.start();
 			}
 			
 		}catch(Exception e){
--- java-workspace/JavaLab/src/echo/EchoServerThread.java
+++ java-workspace/JavaLab/src/echo/EchoServerThread.java
@@ -1,6 +1,8 @@
 package echo;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -9,9 +11,21 @@ public class EchoServerThread extends Thread{
 	private BufferedReader in;
 	private PrintWriter out;
 	
+	public EchoServerThread(Socket s2) {
+		this.s = s2;
+	}
+
 	public void run(){
 		try{
+			// 소켓에서 입출력 스트림 생성
+			in = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+			out = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"), true);// auto flush			
 			
+			// 입력 스트림에서 읽고 출력 스트림으로 전송한다.
+			String readData = "";
+			while((readData = in.readLine()) != null) {
+				out.println(readData);
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
